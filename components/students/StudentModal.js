de3bdf87c701a76e18a8f81e5f4a05b2285cfa58
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-03-17 02:59:55 +0500

    cahnges

--- components/students/StudentModal.js
+++ components/students/StudentModal.js
@@ -4,42 +4,57 @@ var studentSchema =
         {
             id: '000000000',
             title: 'Call carpenter to fix kitchen pipe',
+            homeStatus:false,
             home: 'Home',
             chores: 'Chores',
+            time:false,
+            diamond:true,
             celebration: "Celebrations",
             checkbox:false
         },
         {
             id: '111111111',
             title: 'Pick Melanie from football game',
+            homeStatus:true,
             home: 'Home',
             chores: 'Chores',
+            time:false,
+            diamond:true,
             celebration: "Celebrations",
             checkbox:false
         },
         {
             id: '222222222',
             title: 'Mow the lawn',
+            homeStatus:true,
             home: 'Home',
             chores: 'Chores',
+            time:true,
+            diamond:false,
             celebration: "Celebrations",
             checkbox:false
         },
         {
             id: '3333333333',
             title: 'Help Andreas with homework',
+            homeStatus:false,
             home: 'Home',
             chores: 'Chores',
+            time:false,
+            diamond:true,
             celebration: "Celebrations",
             checkbox:false
         },
         {
             id: '4444444444',
             title: 'Pick Melanie from football game',
+            homeStatus:true,
             home: 'Home',
             chores: 'Chores',
+            time:true,
+            diamond:true,
             celebration: "Celebrations",
-            checkbox:false
+            checkbox:true
         },
 
 
